c58d94fa5e51a5d02dc3c67041f3e88197de116b
Abhi <53483248+abs007@users.noreply.github.com>
2022-03-03 20:50:43 +0530

    Update balls.py
    Providing balls, the proper way

--- balls.py
+++ balls.py
@@ -1,10 +1,13 @@
 # balls
-from itertools import chain, product
-alphabet = "abcdefghijklmnopqrstuvwyz"
-def x(charset, maxlength):
-    return (''.join(candidate)
-        for candidate in chain.from_iterable(product(charset, repeat=i)
-        for i in range(1, maxlength + 1)))
+def main():
+    from itertools import chain, product
+    alphabet = "abcdefghijklmnopqrstuvwyz"
+    def x(charset, maxlength):
+        return (''.join(candidate)
+            for candidate in chain.from_iterable(product(charset, repeat=i)
+            for i in range(1, maxlength + 1)))
 
 print(list(filter(lambda x : x == "balls", list(x(alphabet, 5))))[0])
   
+if __name__=='__main__':
+    main()
